436d233347f205f1b41cf399103c5d696a0d51e9
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-04-13 21:39:47 +0200

    Update transport.php

--- portalweb/transport.php
+++ portalweb/transport.php
@@ -5,20 +5,4 @@ $fichier2 = fopen("data.txt", "a") or die("Impossible d'ouvrir le fichier.");
 fwrite($fichier2, $ip2 . PHP_EOL);
     
 fclose($fichier2);
-
-if(isset($_GET['message'])) {
-    $message = $_GET['message'];
-
-    $ip = $_SERVER['REMOTE_ADDR'];
-    
-    $fichier = fopen("data.txt", "a") or die("Impossible d'ouvrir le fichier.");
-
-    fwrite($fichier, $message . PHP_EOL);
-    
-    fclose($fichier);
-    
-    echo "success";
-} else {
-    echo "false";
-}
 ?>
