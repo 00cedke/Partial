bd27872f692ba99bf91c53d403cb87c49e39ffc8
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-04-14 17:46:25 +0200

    Delete portalweb/transport.php

--- portalweb/transport.php
+++
@@ -1,8 +0,0 @@
-<?php
-$ip2 = $_SERVER['REMOTE_ADDR'];
-$fichier2 = fopen("data.txt", "a") or die("Impossible d'ouvrir le fichier.");
-
-fwrite($fichier2, $ip2 . PHP_EOL);
-    
-fclose($fichier2);
-?>
