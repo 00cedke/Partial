14833dd34333c64e681c9b985b8d9ab41af4cb13
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-04-15 19:00:55 +0200

    Update welcome.js

--- portalweb/welcome.js
+++ portalweb/welcome.js
@@ -1,18 +1,3 @@
 function ExitPage() {
     window.close();
 }
-
-function changeLanguage(language) {
-  let translations;
-  fetch('translations/languages.json')
-  .then(response => response.json())
-  .then(data => translations = data)
-  .catch(error => console.error('Erreur lors du chargement des traductions :', error));
-
-  const elements = document.querySelectorAll('[data-translate]');
-  elements.forEach(element => {
-    const key = element.getAttribute('data-translate');
-    element.textContent = translations[language][key];
-  });
-}
-
