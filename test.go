44678e7cfb0f7d53d248f4009b3803b3d3bbcd53
cedkeChat01 <123301168+00cedke@users.noreply.github.com>
2024-04-19 07:53:19 +0200

    Update test.go

--- test.go
+++ test.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+    "net/http"
+
+    "github.com/labstack/echo/v4"
+
+    "fmt"
+)
 
 func main() {
+    e := echo.New()
+
+    e.GET("/", func(c echo.Context) error {
+        ip := c.RealIP()
+
+        return c.String(http.StatusOK, "IP Addr : "+ip)
+    })
+
+    e.Start(":8080")
+
     fmt.Println("Hello, World!")
 }
