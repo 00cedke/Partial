38a68f8f93bbdb7b02009a758067cebad3942f92
cedkeChat01 <123301168+00cedke@users.noreply.github.com>
2024-04-19 08:23:11 +0200

    Update data-type.ts

--- data-type.ts
+++ data-type.ts
@@ -1,3 +1,22 @@
-//data
-//data
-//data
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('data user is -> %d!');
+});
+
+app.get('/hello/:name', (req: Request, res: Response) => {
+    const name = req.params.name;
+    res.send(`Hello, ${name}!`);
+});
+
+app.post('/hello', (req: Request, res: Response) => {
+    const { name } = req.body;
+    res.send(`Hello, ${name}!`);
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
