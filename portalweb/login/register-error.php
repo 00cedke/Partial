fe12d8bbeb74b6b296ae7479779c73e442736f32
cedkeChat01 <123301168+00cedke@users.noreply.github.com>
2024-04-23 12:04:15 +0200

    Update register-error.php

--- portalweb/login/register-error.php
+++ portalweb/login/register-error.php
@@ -1,1 +1,24 @@
+<?php
 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PartialVerse</title>
+	<link rel="stylesheet" type="text/css" href="css/signup.css">
+</head>
+<body>
+<audio loop autoplay>
+    <source src="audio/bgm.mp3">
+</audio>
+<div class="center">
+	<h1>Before starting,</h1>
+	<h2>Are you a new user or an existing user?</h2>
+	<a href="register.php"><img src="assets/newuser_bt.png" class="centernew"></a>
+	<a href="login.php"><img src="assets/exuser_bt.png" class="centerold"></a>
+</div>	
+</body>
+</html>
